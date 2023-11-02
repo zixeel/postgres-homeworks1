0d3d5681ca22363ea319dbcab9c76620c5c5563b
zixeel <strpan549@gmail.com>
2023-11-02 22:16:12 +0300

    2 homework complete

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -1,14 +1,30 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. заказы, доставленные в страны France, Germany, Spain (таблица orders, колонка ship_country)
-SELECT ...
+SELECT * FROM orders
+WHERE ship_country IN ('France', 'Germany', 'Spain')
 
--- 2. уникальные страны и города, куда отправлялись заказы, отсортировать по странам и городам (таблица orders, колонки ship_country, ship_city)
-
-
--- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
-
-
--- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
+-- 2. уникальные страны и города, куда отправлялись заказы, отсортировать по странам и городам
+ --(таблица orders, колонки ship_country, ship_city)
+SELECT DISTINCT ship_country,  ship_city FROM orders
+ORDER BY ship_country, ship_city
 
 
--- 5. минимальную и максимальную цену среди продуктов, не снятых с продажи и которых имеется не меньше 20 (таблица products, колонки unit_price, units_in_stock, discontinued не равно 1)
+-- 3. сколько дней в среднем уходит на доставку товара в Германию
+--(таблица orders, колонки order_date, shipped_date, ship_country)
+SELECT AVG(shipped_date - order_date) AS avg_ship_dur
+FROM orders
+WHERE ship_country IN ('Germany')
+
+-- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи
+--(таблица products, колонки unit_price, discontinued не равно 1)
+SELECT MIN(unit_price), MAX (unit_price)
+FROM products
+WHERE discontinued <> 1
+
+
+-- 5. минимальную и максимальную цену среди продуктов,
+--не снятых с продажи и которых имеется не меньше 20
+--(таблица products, колонки unit_price, units_in_stock, discontinued не равно 1)
+SELECT MIN(unit_price), MAX (unit_price)
+FROM products
+WHERE discontinued <> 1 AND units_in_stock > 20
--- homework-2/groupby.sql
+++ homework-2/groupby.sql
@@ -1,20 +1,58 @@
 -- Напишите запросы, которые выводят следующую информацию:
--- 1. заказы, отправленные в города, заканчивающиеся на 'burg'. Вывести без повторений две колонки (город, страна) (см. таблица orders, колонки ship_city, ship_country)
+-- 1. заказы, отправленные в города, заканчивающиеся на 'burg'.
+--Вывести без повторений две колонки (город, страна)
+--(см. таблица orders, колонки ship_city, ship_country)
+SELECT ship_city, ship_country
+FROM orders
+WHERE ship_city LIKE '%burg'
+
+-- 2. из таблицы orders идентификатор заказа, идентификатор заказчика,
+-- вес и страну отгрузки. Заказ отгружен в страны, начинающиеся на 'P'.
+--Результат отсортирован по весу (по убыванию). Вывести первые 10 записей.
+SELECT order_id, customer_id, freight, ship_country
+FROM orders
+WHERE ship_country LIKE 'P%'
+ORDER BY freight DESC
+LIMIT 10
 
 
--- 2. из таблицы orders идентификатор заказа, идентификатор заказчика, вес и страну отгрузки. Заказ отгружен в страны, начинающиеся на 'P'. Результат отсортирован по весу (по убыванию). Вывести первые 10 записей.
+-- 3. фамилию, имя и телефон сотрудников, у которых в данных отсутствует регион
+--(см таблицу employees)
+--!!!!!!!тут регион в селекте был не обязателен добавил что бы понять верно сработало или нет !!!!!
+SELECT last_name, first_name, home_phone, region
+FROM employees
+WHERE region IS null
 
+-- 4. количество поставщиков (suppliers) в каждой из стран.
+--Результат отсортировать по убыванию количества поставщиков в стране
+SELECT country, COUNT(*)
+FROM suppliers
+GROUP BY country
+ORDER BY COUNT(*) DESC
 
--- 3. фамилию, имя и телефон сотрудников, у которых в данных отсутствует регион (см таблицу employees)
+-- 5. суммарный вес заказов (в которых известен регион) по странам,
+--но вывести только те результаты, где суммарный вес на страну больше 2750.
+--Отсортировать по убыванию суммарного веса
+--(см таблицу orders, колонки ship_region, ship_country, freight)
+SELECT  ship_country, SUM(freight)
+FROM orders
+WHERE ship_region IS NOT null
+GROUP BY ship_country
+HAVING SUM(freight) >2749
+ORDER BY SUM(freight) DESC
 
-
--- 4. количество поставщиков (suppliers) в каждой из стран. Результат отсортировать по убыванию количества поставщиков в стране
-
-
--- 5. суммарный вес заказов (в которых известен регион) по странам, но вывести только те результаты, где суммарный вес на страну больше 2750. Отсортировать по убыванию суммарного веса (см таблицу orders, колонки ship_region, ship_country, freight)
-
-
--- 6. страны, в которых зарегистрированы и заказчики (customers) и поставщики (suppliers) и работники (employees).
+-- 6. страны, в которых зарегистрированы и заказчики (customers) и поставщики
+--(suppliers) и работники (employees).
+SELECT country FROM customers
+INTERSECT
+SELECT country FROM suppliers
+INTERSECT
+SELECT country FROM employees;
 
 
 -- 7. страны, в которых зарегистрированы и заказчики (customers) и поставщики (suppliers), но не зарегистрированы работники (employees).
+SELECT country FROM customers
+INTERSECT
+SELECT country FROM suppliers
+EXCEPT
+SELECT country FROM employees;
